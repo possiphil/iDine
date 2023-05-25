a27064c921b416e8442bfe5b7f2f296e52c803af
Philipp Sanktjohanser <venture@mein.gmx>
2023-05-25 18:00:16 +0200

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Philipp Sanktjohanser on 25.05.23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
